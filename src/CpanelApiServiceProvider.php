e7da675bad2af89a80dfa2d18972bdf4d0f41dd7
staffkojinpro <50339268+staffkojinpro@users.noreply.github.com>
2021-10-19 00:58:16 +0900

    first API

---
+++ src/CpanelApiServiceProvider.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace victorapi;
+
+use Illuminate\Support\ServiceProvider;
+use victorapi\cPanel;
+
+class CpanelApiServiceProvider extends ServiceProvider
+{
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->publishes([
+            __DIR__.'/config/cPanel.php' => config_path('cPanel.php'),
+        ], 'config');
+
+        $this->mergeConfigFrom(__DIR__.'/config/cPanel.php', 'cPanel');
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        // $this->app->singleton(cPanel::class);
+        $this->app->bind(cPanel::class);
+        // $this->app->singleton(cPanel::class, function ($app) {
+            // return new CorsService($this->corsOptions(), $app);
+        // });
+        // $this->app->bind(cPanel::class);
+    }
+}
---
+++ src/config/cPanel.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+
+    /*
+     * If set to false, no activities will be saved to the database.
+     */
+    // 'enabled' => env('ACTIVITY_LOGGER_ENABLED', true),
+
+    'host' => env('CPANEL_HOST','123'),
+    'port' => env('CPANEL_PORT',2083),
+    'username' => env('CPANEL_USER','123'),
+    'password' => env('CPANEL_PASSWORD','123')
+
+
+
+];
